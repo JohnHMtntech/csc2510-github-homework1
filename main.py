2f60455ca1222d65f1d8c48b7ba397c9e83b7e3c
John Miller <jhmiller43@tntech.edu>
2026-03-31 20:40:16 -0500

    Added funcJohnHMiller and edited fightClub

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 # Main.py 
 # Originally created by Brandon Vandergriff
-# Ruined by: 
+# Ruined by: John Miller
 
-def funcVandergriff():
-
-    # horse
-    # r means raw string btw
-    print(r"""
-
-                                       ._ o o
-                                       \_`-)|_
-                                    ,""       \ 
-                                  ,"     |   ಠ ಠ. 
-                                ,"      ,-\__    `.
-                              ,"       /     `--._;)
-                            ,"        /
-                          ,"         /
-                    """)
-
+import time
+import urllib.request
 
 def fightClub(): 
-    print("Guys, we're not supposed to talk about this. ")
+    print("Guys, we're supposed to talk about this. ")
+
+#Code borrowed from Sibo on Github
+def updateProgressBarJohnMiller(percent):
+  bar_length = 25  # should be less than 100
+  print('\r', end="", flush=True)
+  print("Downloading Bee Movie Script: [{:{}}] {:>3}%".format('■'*int(percent/(100.0/bar_length)), bar_length, int(percent)), end="", flush=True)
+
+def funcJohnHMiller():
+  data =  urllib.request.urlopen("https://git.lysator.liu.se/catears/beemovie/-/raw/master/beemovie.txt")
+  with open("beeMovieScript.txt", 'w') as writeFile:
+    total = 4563  # total line count
+    currentLine = 1    
+    for line in data:
+      lineText = str(line)
+      lineText = lineText[2:len(lineText)-3]+"\n"
+      writeFile.write(lineText)
+
+      updateProgressBarJohnMiller(100.0*currentLine/total)
+      currentLine = currentLine + 1
+      
+      time.sleep(0.002)
+    updateProgressBarJohnMiller(100.0)
+  print("\nScript sucessfully downloaded")
+  
+
 
 if __name__ == "__main__":
-    funcVandergriff()
     fightClub()
+    funcJohnHMiller()
